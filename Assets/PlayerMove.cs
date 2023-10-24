8c0333507413e186cfdc62ded4d858c57d2e7676
Pablo Mendoza <asdson5555@gmail.com>
2023-10-24 16:05:05 -0500

    Player Movement

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Rigidbody2D rb2d;
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        rb2d.velocity = new Vector2(horizontal, vertical) * speed;
+    }
+}

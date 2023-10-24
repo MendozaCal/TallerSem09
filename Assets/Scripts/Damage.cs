d7411a57c88bba7141e29cefc4ccbd7b84a984a7
RicardoPariona <hepsoricardo@gmail.com>
2023-10-24 17:14:39 -0500

    Danage?

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private int Damagee;
+
+    public int GetDamage()
     {
-        
+        return Damagee;
     }
 }

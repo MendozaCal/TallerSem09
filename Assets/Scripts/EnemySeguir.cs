3fcfd592266d45e1b5c391f0a3937066305665e9
RicardoPARIONA <hepsoricardo@gmail.com>
2023-10-25 19:04:03 -0500

    Correcion EnemySeguir

--- Assets/Scripts/EnemySeguir.cs
+++ Assets/Scripts/EnemySeguir.cs
@@ -10,13 +10,9 @@ public class EnemySeguir : MonoBehaviour
     void Update()
     {
         dISTANCEjUGADOR = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
-        direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         if ((dISTANCEjUGADOR < 4) && gameObject.CompareTag("EnemyUp"))
         {
             transform.position = Vector2.MoveTowards(transform.position, player.transform.position, sPEED * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
 
         }
     }
